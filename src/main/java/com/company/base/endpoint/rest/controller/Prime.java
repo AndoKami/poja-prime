9108986ca919cc0e7a5fcb9dba48631346cdd82f
AndoKami <hei.ando.1007@gmail.com>
2023-12-12 20:43:21 +0300

    feat: trying to implement the code with random

--- src/main/java/com/company/base/endpoint/rest/controller/Prime.java
+++ src/main/java/com/company/base/endpoint/rest/controller/Prime.java
@@ -6,13 +6,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.math.BigInteger;
 import java.security.SecureRandom;
+import java.util.Random;
 
 @RestController
 @RequestMapping("/Prime")
 public class Prime {
     @GetMapping
     public String getPrime() {
-        BigInteger probablePrime = BigInteger.probablePrime(10000, new SecureRandom());
+        BigInteger probablePrime = BigInteger.probablePrime(10000, new Random());
         return probablePrime.toString();
     }
 }
